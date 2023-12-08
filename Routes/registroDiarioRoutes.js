282f62c3c62178c1316e961ab7f0daccc716d6b7
LuizCPorto <luiz.porto@mail.uft.edu.br>
2023-12-08 05:32:56 -0300

    Add a parte de ordenação

--- Routes/registroDiarioRoutes.js
+++ Routes/registroDiarioRoutes.js
@@ -6,6 +6,7 @@ const {
   findRegsDiario,
   updateRegsDiario,
   deleteRegsDiario,
+  findRegsByKeyword,
 } = registroDiarioController;
 const router = express.Router();
 
@@ -17,14 +18,24 @@ router.delete("/registroDiario/:id", deleteRegsDiario);
 router.get("/registrosDiario", (req, res) => {
   const diarioId = req.query.diarioId;
 
-  if (!diarioId){
+  if (!diarioId) {
     return res.status(400).send({
       message: 'diarioId is required in query parameters.'
     });
   }
 
-  findAllRegsDiario(diarioId, res);
+  const orderBy = req.query.orderBy; // Receber o parâmetro de ordenação
+
+  if (orderBy && (orderBy !== 'date' && orderBy !== 'title')) {
+    return res.status(400).send({
+      message: 'Invalid orderBy parameter. Use "date" or "title".'
+    });
+  }
+
+  findAllRegsDiario(diarioId, orderBy, res);
 });
 
+// Rota para busca por palavra-chave
+router.get("/registrosDiario/search", findRegsByKeyword);
 
 module.exports = router;
